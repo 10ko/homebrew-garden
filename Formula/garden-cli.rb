da17c848431f13fb08ef187882520f62765b1886
runner <runner@Mac-7.local>
2019-09-16 15:55:23 +0000

    update to 0.11.0

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,8 +1,8 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://github.com/garden-io/garden/releases/download/v0.10.9/garden-v0.10.9-macos-amd64.tar.gz"
-  version "0.10.9"
+  url "https://github.com/10ko/garden/releases/download/v0.11.0/garden-v0.11.0-macos-amd64.tar.gz"
+  version "0.11.0"
   sha256 "a928131d854d924bc3b1a4723e98ced3fd4bc7a6809e5c1e3f516033556c085a"
 
   depends_on "rsync"
